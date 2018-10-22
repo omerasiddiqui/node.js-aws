d8320e49bc849ef4d5812d85c2cdcbd84a818a11
Omer Siddiqui <omer@Omers-MacBook.local>
2018-10-21 18:52:34 -0600

    privacy policy fix

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/privacy', (req, res) => {
-    res.render('/privacy.html');
-})
+    res.sendFile(path.join(__dirname, './privacy.html'));
+   });
 
 app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
