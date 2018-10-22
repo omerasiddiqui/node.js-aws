97ac2d6f71ca0a07bca42adf96e8f0221b6f4f62
Omer Siddiqui <omer@Omers-MacBook.local>
2018-10-21 18:40:42 -0600

    privacy policy

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.get('/privacy', (req, res) => {
+    res.render('privacy');
+})
+
 app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
