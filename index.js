834362b6831a869338143dcae2b7fb82fab1dd78
Omer Siddiqui <omer@Omers-MacBook.local>
2018-10-21 19:07:01 -0600

    testing privacy page

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 
 app.get('/privacy', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/privacy.html'));
-   });
+    res.send('<h1>Privacy Policy</h1>');
+});
 
 app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
