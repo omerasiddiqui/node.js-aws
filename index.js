cfb433b673f61e442835e441623c14b4d1f1ac8d
Omer Siddiqui <omer@Omers-MacBook.local>
2018-10-22 19:22:33 -0600

    route for paypal donation button

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ app.get('/privacy', (req, res) => {
     res.sendFile(path.join(__dirname, '../current/public', 'privacy.html'));
 });
 
+app.get('donate-button', (req, res) => {
+    res.sendFile(path.join(__dirname, '../current/public/assets/images/', 'paypal-button.png')
+})
+
 app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
