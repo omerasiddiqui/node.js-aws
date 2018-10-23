6d11f38c601991471c9ece96fedf7ff891da798d
Omer Siddiqui <omer@Omers-MacBook.local>
2018-10-22 19:26:18 -0600

    /donate-button route fix

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.get('/privacy', (req, res) => {
 });
 
 app.get('/donate-button', (req, res) => {
-    res.sendFile(path.join(__dirname, '../current/public/assets/images/', 'paypal-button.png')
-})
+    res.sendFile(path.join(__dirname, '../current/public/assets/images/', 'paypal-button.png'));
+});
 
 app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
