ff0dd716f500eb194a68c9f8d6549057944df285
Omer Siddiqui <34426399+omerasiddiqui@users.noreply.github.com>
2021-10-16 09:24:21 -0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-3-142-184-105.us-east-2.compute.amazonaws.com',
+      host: 'ec2-18-116-25-71.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/icfla2.pem',
       ref: 'origin/master',
       repo: 'https://github.com/omerasiddiqui/node.js-aws.git',
